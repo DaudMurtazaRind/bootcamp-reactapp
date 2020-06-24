22c57a81e43b202d0996d9da7a4e9e7607ae46c5
Daud Murtaza Rind <daud21111987@gmail.com>
2020-06-24 18:20:12 +0500

    initial commit

---
+++ build/precache-manifest.5a2b006fb5587360d4e87e3ee6fd73df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c58807a05f83338a8f840599e934514",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09b9c983fd7341c7003e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "386a558d13ff36bff87c",
+    "url": "/static/js/2.85d3b715.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.85d3b715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09b9c983fd7341c7003e",
+    "url": "/static/js/main.da243092.chunk.js"
+  },
+  {
+    "revision": "70ee4128ee2952f13296",
+    "url": "/static/js/runtime-main.fae1836c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
